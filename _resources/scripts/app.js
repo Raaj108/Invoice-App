bdc5a194cebc514217c69bbd50a20f2a502f1f13
Rajan <rajj108@outlook.com>
2017-01-31 20:37:19 -0800

    merged app.js

--- _resources/scripts/app.js
+++ _resources/scripts/app.js
@@ -1,9 +1,5 @@
 var myApp = angular.module('invoiceApp', []);
 
-<<<<<<< HEAD
-myApp.controller('GreetingController', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
-=======
 app.controller('invoiceController', ['$scope', function ($scope) {
   $scope.customer = {
     customerName: "",
@@ -12,5 +8,4 @@ app.controller('invoiceController', ['$scope', function ($scope) {
     email: "",
     phones: ""
   };
->>>>>>> ef07978db07f8b400f23851852a7d8a4121c77cd
 }]);
