f2f1de9f641c85f5badc4eff437fe300d76179be
Rajan <rajj108@outlook.com>
2017-01-31 23:12:41 -0800

    added product table, and scope

--- _resources/scripts/app.js
+++ _resources/scripts/app.js
@@ -1,11 +1,24 @@
 var myApp = angular.module('invoiceApp', []);
-
-app.controller('invoiceController', ['$scope', function ($scope) {
-  $scope.customer = {
-    customerName: "",
-    company: "",
-    address: "",
-    email: "",
-    phones: ""
-  };
+myApp.controller('invoiceController', ['$scope', function ($scope) {
+    $scope.customer = {
+        customerName: ""
+        , company: ""
+        , address: ""
+        , email: ""
+        , phones: ""
+    };
+    $scope.greaterThan = function (prop, val) {
+        return function (item) {
+            return item[prop] > val;
+        }
+    };
+    $scope.products = {
+        serialNumbers: 0
+    };
+    $scope.addRow = function () {
+        $scope.products.serialNumbers++;
+        console.log($scope.products.serialNumbers);
+    };
+    $scope.today = new Date();
+    console.log($scope.today);
 }]);
