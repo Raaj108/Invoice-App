bdd347c3dd0cb7bd49d936dd1ad9021fa41c7004
Raaj108 <rajj108@outlook.com>
2017-02-01 20:29:08 -0800

    new code

--- _resources/scripts/app.js
+++ _resources/scripts/app.js
@@ -1,24 +1,48 @@
 var myApp = angular.module('invoiceApp', []);
 myApp.controller('invoiceController', ['$scope', function ($scope) {
-    $scope.customer = {
-        customerName: ""
-        , company: ""
-        , address: ""
-        , email: ""
-        , phones: ""
-    };
-    $scope.greaterThan = function (prop, val) {
-        return function (item) {
-            return item[prop] > val;
-        }
-    };
-    $scope.products = {
-        serialNumbers: 0
-    };
-    $scope.addRow = function () {
-        $scope.products.serialNumbers++;
-        console.log($scope.products.serialNumbers);
-    };
-    $scope.today = new Date();
-    console.log($scope.today);
+  $scope.customer = {
+    customerName: "",
+    company: "",
+    address: "",
+    email: "",
+    phones: "",
+    products: [{
+      serialNumbers: 0,
+      description: "",
+      quantity: 0,
+      rate: 0,
+      totalPrice: 0
+    }]
+  };
+
+  $scope.serialNumbers = 1;
+  $scope.description = "";
+  $scope.quantity = 0;
+  $scope.rate = 0.00;
+  $scope.totalPrice = 0.00;
+
+  $scope.addRow = function () {
+
+    $scope.customer.products.push({
+      'serialNumbers': $scope.serialNumbers,
+      'description': $scope.description,
+      'quantity': $scope.quantity,
+      'rate': $scope.rate,
+      'totalPrice': $scope.totalPrice,
+    });
+
+    $scope.serialNumbers++;
+
+    console.log($scope.customer.products);
+
+  }
+
+  $scope.greaterThan = function (prop, val) {
+    return function (item) {
+      return item[prop] > val;
+    }
+  };
+
+  $scope.today = new Date();
+  console.log($scope.today);
 }]);
