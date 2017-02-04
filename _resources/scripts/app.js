ddd9138c7e53cebccd1469158c99dc201e252bd6
Raaj108 <rajj108@outlook.com>
2017-02-03 16:17:21 -0800

    modified app.js and index.js for add/remove function

--- _resources/scripts/app.js
+++ _resources/scripts/app.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('invoiceApp', []);
-myApp.controller('invoiceController', ['$scope', function ($scope) {
+myApp.controller('invoiceController', ['$scope', '$window', '$log', function ($scope, $window, $log) {
+
+  $scope.today = new Date();
+
+
+  $scope.serialNumbers = 1;
   $scope.customer = {
     customerName: "",
     company: "",
@@ -15,34 +20,38 @@ myApp.controller('invoiceController', ['$scope', function ($scope) {
     }]
   };
 
-  $scope.serialNumbers = 1;
-  $scope.description = "";
-  $scope.quantity = 0;
-  $scope.rate = 0.00;
-  $scope.totalPrice = 0.00;
-
-  $scope.addRow = function () {
-
-    $scope.customer.products.push({
-      'serialNumbers': $scope.serialNumbers,
-      'description': $scope.description,
-      'quantity': $scope.quantity,
-      'rate': $scope.rate,
-      'totalPrice': $scope.totalPrice,
-    });
-
-    $scope.serialNumbers++;
-
-    console.log($scope.customer.products);
-
+  //calcutale Total price
+  var quantity = Number($scope.customer.products.quantity || 0);
+  var rate = Number($scope.customer.products.rate || 0);
+  $scope.AddNumbers = function (customer) {
+    $scope.totalPrice = quantity + rate;
   }
 
-  $scope.greaterThan = function (prop, val) {
-    return function (item) {
-      return item[prop] > val;
-    }
-  };
+  //add a new row
+  $scope.addRow = function (customer) {
+    $scope.customer.products.push({
+      'serialNumbers': $scope.serialNumbers,
+      'description': '',
+      'quantity': quantity,
+      'rate': rate,
+      'totalPrice': $scope.totalPrice,
+    });
+    $scope.serialNumbers++
+      console.log($scope.customer.products);
+  }
 
-  $scope.today = new Date();
-  console.log($scope.today);
+  $scope.removeRow = function (event) {
+
+    var selectRemoveRow = angular.element(event.target).parent().parent();
+    var removeSerialNumber = angular.element(selectRemoveRow).find('input[id="serialNumber"]').val();
+
+    angular.forEach($scope.customer.products, function (value, key) {
+      if ($scope.customer.products[key].serialNumbers == removeSerialNumber) {
+        console.log(key);
+      }
+
+    });
+    angular.element(selectRemoveRow).remove()
+    console.log($scope.customer.products);
+  }
 }]);
