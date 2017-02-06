d4d27fc664397a8e0822dc3af53ba9b7f8e7e4c3
Rajan <rajj108@outlook.com>
2017-02-05 23:58:19 -0800

    modified logic to add remove the rows

--- _resources/scripts/app.js
+++ _resources/scripts/app.js
@@ -1,57 +1,60 @@
 var myApp = angular.module('invoiceApp', []);
 myApp.controller('invoiceController', ['$scope', '$window', '$log', function ($scope, $window, $log) {
-
-  $scope.today = new Date();
-
-
-  $scope.serialNumbers = 1;
-  $scope.customer = {
-    customerName: "",
-    company: "",
-    address: "",
-    email: "",
-    phones: "",
-    products: [{
-      serialNumbers: 0,
-      description: "",
-      quantity: 0,
-      rate: 0,
-      totalPrice: 0
-    }]
-  };
-
-  //calcutale Total price
-  var quantity = Number($scope.customer.products.quantity || 0);
-  var rate = Number($scope.customer.products.rate || 0);
-  $scope.AddNumbers = function (customer) {
-    $scope.totalPrice = quantity + rate;
-  }
-
-  //add a new row
-  $scope.addRow = function (customer) {
-    $scope.customer.products.push({
-      'serialNumbers': $scope.serialNumbers,
-      'description': '',
-      'quantity': quantity,
-      'rate': rate,
-      'totalPrice': $scope.totalPrice,
-    });
-    $scope.serialNumbers++
-      console.log($scope.customer.products);
-  }
-
-  $scope.removeRow = function (event) {
-
-    var selectRemoveRow = angular.element(event.target).parent().parent();
-    var removeSerialNumber = angular.element(selectRemoveRow).find('input[id="serialNumber"]').val();
-
-    angular.forEach($scope.customer.products, function (value, key) {
-      if ($scope.customer.products[key].serialNumbers == removeSerialNumber) {
-        console.log(key);
-      }
-
-    });
-    angular.element(selectRemoveRow).remove()
-    console.log($scope.customer.products);
-  }
+    $scope.today = new Date();
+    $scope.serialNumbers = 1;
+    $scope.customer = {
+        customerName: ""
+        , company: ""
+        , address: ""
+        , email: ""
+        , phones: ""
+    };
+    $scope.products = [{
+        serialNumbers: 0
+        , description: ""
+        , quantity: 0
+        , rate: 0
+        , totalPrice: 0
+    }];
+    $scope.totalPrice = 0;
+    $scope.serialNumbers = 1;
+    $scope.description = "";
+    $scope.quantity = 0;
+    $scope.rate = 0;
+    //calcutale Total price
+    $scope.calculateTotal = function () {
+            $scope.totalPrice = parseFloat($scope.quantity) * parseFloat($scope.rate);
+        }
+        //add a new row
+    $scope.addRow = function (product) {
+        $scope.products.push({
+            serialNumbers: $scope.serialNumbers
+            , description: $scope.description
+            , quantity: $scope.quantity
+            , rate: $scope.rate
+            , totalPrice: $scope.totalPrice
+        });
+        $scope.serialNumbers++;
+        $scope.totalPrice = 0;
+        $scope.description = "";
+        $scope.quantity = 0;
+        $scope.rate = 0;
+        $log.info($scope.products);
+    }
+    $scope.removeRow = function (event) {
+        var selectRemoveRow = angular.element(event.target).parent().parent();
+        var removeSerialNumber = angular.element(selectRemoveRow).find('td#serialNumber').val();
+        angular.forEach($scope.products, function (value, key) {
+            if ($scope.products[key].serialNumbers == removeSerialNumber) {
+                $scope.products.splice(key, 1);
+            }
+        });
+        angular.element(selectRemoveRow).remove();
+        $scope.serialNumbers--;
+        $scope.totalPrice = 0;
+        $scope.description = "";
+        $scope.quantity = 0;
+        $scope.rate = 0;
+        $log.info($scope.products);
+    }
 }]);
