03368ff1dfb476b540cd5d8fa3a464b85694e368
Raaj108 <rajj108@outlook.com>
2017-02-06 15:51:10 -0800

    modified remove logic and added print button

--- _resources/scripts/app.js
+++ _resources/scripts/app.js
@@ -1,60 +1,95 @@
 var myApp = angular.module('invoiceApp', []);
 myApp.controller('invoiceController', ['$scope', '$window', '$log', function ($scope, $window, $log) {
-    $scope.today = new Date();
-    $scope.serialNumbers = 1;
-    $scope.customer = {
-        customerName: ""
-        , company: ""
-        , address: ""
-        , email: ""
-        , phones: ""
-    };
-    $scope.products = [{
-        serialNumbers: 0
-        , description: ""
-        , quantity: 0
-        , rate: 0
-        , totalPrice: 0
+
+  $scope.today = new Date();
+
+  $scope.customer = {
+    customerName: "",
+    company: "",
+    address: "",
+    email: "",
+    phones: ""
+  };
+
+  $scope.products = [{
+    serialNumbers: 0,
+    description: "",
+    quantity: 0,
+    rate: 0,
+    totalPrice: 0
     }];
+
+  $scope.grandTotalPrice = 0;
+  $scope.totalPrice = 0;
+  $scope.serialNumbers = 1;
+  $scope.description = "";
+  $scope.quantity = 0;
+  $scope.rate = 0;
+
+  //calcutale Total price
+  $scope.calculateTotal = function () {
+    $scope.totalPrice = parseFloat($scope.quantity) * parseFloat($scope.rate);
+  }
+
+  //add a new row
+  $scope.addRow = function (product) {
+    $scope.products.push({
+      serialNumbers: $scope.serialNumbers,
+      description: $scope.description,
+      quantity: $scope.quantity,
+      rate: $scope.rate,
+      totalPrice: $scope.totalPrice
+    });
+    $scope.serialNumbers++;
     $scope.totalPrice = 0;
-    $scope.serialNumbers = 1;
     $scope.description = "";
     $scope.quantity = 0;
     $scope.rate = 0;
-    //calcutale Total price
-    $scope.calculateTotal = function () {
-            $scope.totalPrice = parseFloat($scope.quantity) * parseFloat($scope.rate);
-        }
-        //add a new row
-    $scope.addRow = function (product) {
-        $scope.products.push({
-            serialNumbers: $scope.serialNumbers
-            , description: $scope.description
-            , quantity: $scope.quantity
-            , rate: $scope.rate
-            , totalPrice: $scope.totalPrice
-        });
-        $scope.serialNumbers++;
-        $scope.totalPrice = 0;
-        $scope.description = "";
-        $scope.quantity = 0;
-        $scope.rate = 0;
-        $log.info($scope.products);
-    }
-    $scope.removeRow = function (event) {
-        var selectRemoveRow = angular.element(event.target).parent().parent();
-        var removeSerialNumber = angular.element(selectRemoveRow).find('td#serialNumber').val();
-        angular.forEach($scope.products, function (value, key) {
-            if ($scope.products[key].serialNumbers == removeSerialNumber) {
-                $scope.products.splice(key, 1);
-            }
-        });
-        angular.element(selectRemoveRow).remove();
-        $scope.serialNumbers--;
-        $scope.totalPrice = 0;
-        $scope.description = "";
-        $scope.quantity = 0;
-        $scope.rate = 0;
-        $log.info($scope.products);
-    }
+    $log.info($scope.products);
+    $scope.calculateGrandTotal($scope.products);
+  }
+
+  //ramove a row
+  $scope.removeRow = function (event, index) {
+    var selectRemoveRow = angular.element(event.target).parent().parent();
+
+    $scope.products.splice(index, 1);
+
+    angular.element(selectRemoveRow).remove();
+    $scope.serialNumbers--;
+    $scope.totalPrice = 0;
+    $scope.description = "";
+    $scope.quantity = 0;
+    $scope.rate = 0;
+    $log.info($scope.products);
+    $scope.calculateGrandTotal($scope.products);
+  }
+
+  //calculate grand total price
+  $scope.calculateGrandTotal = function (products) {
+    $scope.grandTotal = 0;
+    angular.forEach(products, function (value, key) {
+      $scope.grandTotal = $scope.grandTotal + products[key].totalPrice;
+    });
+    $scope.grandTotalPrice = $scope.grandTotal;;
+  }
+
+  //Print
+  $scope.printData = function () {
+    var divToPrint = document.getElementById("printTable");
+    newWin = window.open("");
+    newWin.document.write(divToPrint.outerHTML);
+    newWin.print();
+    newWin.close();
+  }
 }]);
+
+
+myApp.filter('titleCase', function () {
+  return function (input) {
+    input = input || '';
+    return input.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
+})
