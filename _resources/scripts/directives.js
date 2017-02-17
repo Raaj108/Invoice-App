e411d917ae5eecfb29e88cb89eb88e26963673ea
Raaj108 <rajj108@outlook.com>
2017-02-16 16:52:15 -0800

    divided app into modules

---
+++ _resources/scripts/directives.js
@@ -0,0 +1,29 @@
+//Directives
+angular.module('invoiceApp')
+  .directive('customerInfo', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: 'templates/_partial_customer_info.html'
+    }
+  })
+  .directive('addRow', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: 'templates/_partial_addRow.html',
+      replace: true
+    }
+  })
+  .directive('tools', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: 'templates/_partial_tools.html',
+      replace: true
+    }
+  })
+  .directive('invoiceTable', function () {
+    return {
+      restrict: 'EA',
+      templateUrl: 'templates/_partial_invoice_table.html',
+      replace: true
+    }
+  });
---
+++ _resources/scripts/filters.js
@@ -0,0 +1,9 @@
+//Filters
+angular.module('invoiceApp').filter('titleCase', function () {
+  return function (input) {
+    input = input || '';
+    return input.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
+});
